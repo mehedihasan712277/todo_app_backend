52889c8997949343fcb12f95905cd937a8e78dcc
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-12-29 11:19:38 +0600

    schema created

---
+++ src/db/models/todo.models.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+export const Todo = mongoose.model("Todo", todoSchema);
+
